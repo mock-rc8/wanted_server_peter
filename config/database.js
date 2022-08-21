ec71b7b146328bfddd4d869a7d9aa2b870062138
MIN0225 <cro77@naver.com>
2022-08-21 22:21:28 +0900

    2022-08-21 reademe update
    8월21일 일요일 reademe 진행상황 업데이트

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
+    host: 'peter-risingtest.cajqpx1kwz79.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
     port: '3306',
-    password: '',
-    database: ''
+    password: '12345678',
+    database: 'wanted'
 });
 
 module.exports = {
